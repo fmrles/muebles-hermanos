ddb70511e4824b2782b303595d23f409267b1957
Francisco <f.jesusmorales.m@gmail.com>
2025-11-01 13:50:11 -0300

    init

---
+++ src/main/java/com/evaluacion2/muebles_hermanos/MueblesHermanosApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion2.muebles_hermanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueblesHermanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblesHermanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion2/muebles_hermanos/MueblesHermanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion2.muebles_hermanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesHermanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
